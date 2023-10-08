f7e6068633c82f9333cbebd3c0696d63ca71a3e4
NexusI9 <nassim.elkhantour@gmail.com>
2023-10-08 22:06:06 +0200

    setup react

---
+++ src/client/index.tsx
@@ -0,0 +1,10 @@
+//   parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')import React from 'react';
+
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const container = document.getElementById('powerui-app');
+  const root = createRoot(container);
+  root.render(<App />);
+});
---
+++ src/server/index.ts
@@ -0,0 +1,35 @@
+// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+
+
+figma.showUI(__html__);
+
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = msg => {
+
+  switch (msg.type) {
+
+    case "create-rectangles":
+
+      const nodes: SceneNode[] = [];
+      for (let i = 0; i < msg.count; i++) {
+        const rect = figma.createRectangle();
+        rect.x = i * 150;
+        rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+        figma.currentPage.appendChild(rect);
+        nodes.push(rect);
+      }
+      figma.currentPage.selection = nodes;
+      figma.viewport.scrollAndZoomIntoView(nodes);
+
+      break; 
+
+    default:
+
+  }
+
+// Make sure to close the plugin when you're done. Otherwise the plugin will
+// keep running, which shows the cancel button at the bottom of the screen.
+figma.closePlugin();
+};
