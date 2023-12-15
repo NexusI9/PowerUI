3e81a866c4168403ff0f1c5ce488a7461b66ea0f
NexusI9 <nassim.elkhantour@gmail.com>
2023-12-15 23:06:35 +0100

    continue folder

--- src/client/components/folder/index.tsx
+++ src/client/components/folder/index.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import './index.scss';
 import { Folder as FolderInterface } from '@lib/interfaces';
+import { OptionsRow } from '@components/options-row';
+import Move from '@icons/mobile.svg';
+import Carrot from '@icons/carrot.svg'
 
-export const Folder = ({title, options, children}: FolderInterface) => {
-
+export const Folder = ({ title, options, children }: FolderInterface) => {
+    <div className='folder'>
+        <div className='folder-header flex f-row'></div>
+        <div className='folder-body'></div>
+        <div className='folder-footer'></div>
+    </div>
 
 };
