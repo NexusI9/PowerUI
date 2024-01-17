274600b60dc474de8d0115108147334ad2fab950
NexusI9 <nassim.elkhantour@gmail.com>
2024-01-17 23:15:22 +0100

    clean up and uniformise templates

--- src/client/templates/export/index.tsx
+++ src/client/templates/export/index.tsx
@@ -5,19 +5,20 @@ import { destroy } from "@lib/slices/export.template";
 import { FloatingWindow } from "@components/floating-window";
 
 import './index.scss';
+import { ExportContent } from "@components/export-content";
 
 export const Export = () => {
 
     const dispatch = useDispatch();
-    const exportTemplate = useSelector((state: any) => state.export);
+    const template = useSelector((state: any) => state.export);
 
     return (
         <FloatingWindow
             onDestroy={() => dispatch(destroy())}
-            template={exportTemplate}
+            template={template}
         >
-            <Sidepanel {...exportTemplate} />
-
+            <Sidepanel {...template} />
+            <ExportContent {...template} />
         </FloatingWindow>
     );
 
