aaeb4b9b47fd21b6abd263cf33cfe77108aaf541
NexusI9 <nassim.elkhantour@gmail.com>
2024-01-18 16:01:31 +0100

    set color exports

--- src/client/components/dev-content/index.tsx
+++ src/client/components/dev-content/index.tsx
@@ -1,12 +1,12 @@
 import { Dev } from "@ctypes/dev.template";
 import './index.scss';
+import { BaseSyntheticEvent } from "react";
 
 export const DevContent = (props: Dev) => {
 
     console.log(props.code);
     return (
-        <code className="dev-content paper full-width ">
-            {props.code}
-        </code>
+        <textarea readOnly={true} className="dev-content paper full-width" defaultValue={props.code} onClick={ (e:BaseSyntheticEvent) =>{e.target.focus(); e.target.select();}}>
+        </textarea>
     );
 }
