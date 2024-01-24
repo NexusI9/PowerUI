f48592dd67546d73819a61ac9aec37cce4c72174
NexusI9 <nassim.elkhantour@gmail.com>
2024-01-24 12:03:53 +0100

    set tooltip font

--- src/client/components/font/index.tsx
+++ src/client/components/font/index.tsx
@@ -8,6 +8,7 @@ import { FontOptions } from '@components/font-options';
 import { loadFont } from '@lib/utils/font.action';
 import { cssTextStyle } from '@lib/utils/font';
 import { TextSet } from '@ctypes/text';
+import { display as displayTooltip, destroy as destroyTooltip } from '@lib/slices/tooltip';
 import { display as displayContextMenu } from '@lib/slices/contextmenu';
 
 export const Font = (props: TextSet) => {
@@ -15,13 +16,14 @@ export const Font = (props: TextSet) => {
     const displayMode = useSelector((state: any) => state.style.display);
     const dispatch = useDispatch();
     const fontLoaded = useRef(false);
+    const styleName = folderNameFromPath(String(props.name)).name;
 
     useEffect(() => {
         if (!fontLoaded.current) {
             loadFont(props.fontName);
             fontLoaded.current = true;
         }
-    },[]);
+    }, []);
 
     const updateName = (e: BaseSyntheticEvent) => {
         send({
@@ -33,6 +35,16 @@ export const Font = (props: TextSet) => {
         });
     }
 
+    const handleToolTip = (ref: any) => {
+        const { x, y, width, height } = ref.getBoundingClientRect();
+        dispatch(displayTooltip({
+            content: [
+                { type: 'INPUT', value: styleName, action: 'UPDATE_STYLE_NAME', payload: { style: props, name: null } },
+            ],
+            boundingBox: { x, y, width, height }
+        }));
+    }
+
     return (<div
         className="style-item-font flex"
         onContextMenu={(e: any) => {
@@ -41,10 +53,12 @@ export const Font = (props: TextSet) => {
                 position: { x: e.clientX, y: e.clientY }
             }))
         }}
+        onMouseEnter={(e) => displayMode === 'grid' && handleToolTip(e.target)}
+        onMouseLeave={() => dispatch(destroyTooltip())}
     >
         <Input
             {...(displayMode === 'list' && { style: cssTextStyle(props) })}
-            value={folderNameFromPath(String(props?.name)).name}
+            value={styleName}
             appearance={{ minified: true }}
             onBlur={updateName}
             onEnter={updateName}
