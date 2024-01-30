6e7de33bdae47b28bc4f6358bddf78aa518040fc
NexusI9 <nassim.elkhantour@gmail.com>
2024-01-30 19:04:57 +0100

    minor fixes

--- src/client/components/options-row/OptionsRow.tsx
+++ src/client/components/options-row/OptionsRow.tsx
@@ -7,7 +7,7 @@ import { Button as IButton } from '@ctypes/input';
 export default ({ options, className }: { options: MultiArray<IButton>, className?: string }) => (
     <div className={`options-row flex f-row gap-m ${className && className || ''}`}>{
         options.map((option, i) => <div key={JSON.stringify(option) + i} className='flex f-row gap-xs'>
-            {traverseCallback(option, (option: IButton) => <Button {...option} />)}
+            {traverseCallback(option, (option: IButton, i:number) => <Button key={JSON.stringify(option)+i}{...option} />)}
         </div>)
     }</div>
 );
