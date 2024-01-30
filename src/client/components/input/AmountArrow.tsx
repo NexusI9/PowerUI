3a831714546e4a5aa4efc2e585ba6984f45c9d87
NexusI9 <nassim.elkhantour@gmail.com>
2024-01-30 18:57:31 +0100

    minor changes

--- src/client/components/input/AmountArrow.tsx
+++ src/client/components/input/AmountArrow.tsx
@@ -4,7 +4,7 @@ import { Button } from '@components/button';
 
 export const AmountArrows = ({ onUp, onDown }: any) => (
     <label className='input-field-amount flex f-col'>
-        <Button iconLeft={ChevronUp} onClick={onUp || void 0} role='DISABLED' />
-        <Button iconLeft={ChevronDown} onClick={onDown || void 0} role='DISABLED' />
+        <Button iconLeft={ChevronUp} onClick={onUp || void 0} role='GHOST' />
+        <Button iconLeft={ChevronDown} onClick={onDown || void 0} role='GHOST' />
     </label>
 )
